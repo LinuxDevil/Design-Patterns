cad33b5ef56f80a8f8ae14ed8bc47c8cc7a68575
Ali Mohammad <alimohammad@pc-2473.home>
2023-03-04 20:25:14 +0300

    creational: Prototype

--- creational/prototype.swift
+++ creational/prototype.swift
@@ -10,6 +10,9 @@
 // object from scratch.
 
 
+// The Prototype pattern delegates the cloning process
+// to the actual objects that are being cloned. 
+
 // Problem:
 // Suppose you are creating a game that involves
 // creating different types of monsters with different
@@ -17,6 +20,7 @@
 // every time, you can use the Prototype pattern to clone
 // an existing monster and modify its attributes to create
 // a new one.
+
 
 
 // The prototype protocol defines the clone method that will return a copy of the prototype
@@ -69,3 +73,82 @@ let orc2 = spawner.spawnMonster(type: "orc", name: "Krogg")
 
 print(orc1?.name)
 print(orc2?.name) 
+
+// Another Implementation: 
+
+
+
+// NSCopying is a protocol that defines the copy method.
+// The copy method returns a copy of the object.
+// The copy method is used to create a new object that is
+// a copy of the current object.
+class NPC: NSCopying {
+    var name: String
+    var type: String
+    var level: Int
+    
+    init(name: String, type: String, level: Int) {
+        self.name = name
+        self.type = type
+        self.level = level
+    }
+    
+    // NSZone is a class that represents a memory zone. It is
+    // used to allocate memory for objects. The default zone
+    // is the default zone for the current thread.
+    func copy(with zone: NSZone? = nil) -> Any {
+        let copy = type(of: self).init(name: name, type: type, level: level)
+        return copy
+    }
+}
+
+class Human: NPC {
+    var race: String
+    
+    init(name: String, type: String, level: Int, race: String) {
+        self.race = race
+        super.init(name: name, type: type, level: level)
+    }
+    
+    override func copy(with zone: NSZone? = nil) -> Any {
+        let copy = super.copy(with: zone) as! Human
+        copy.race = race
+        return copy
+    }
+}
+
+class Elf: NPC {
+    var magicType: String
+    
+    init(name: String, type: String, level: Int, magicType: String) {
+        self.magicType = magicType
+        super.init(name: name, type: type, level: level)
+    }
+    
+    override func copy(with zone: NSZone? = nil) -> Any {
+        let copy = super.copy(with: zone) as! Elf
+        copy.magicType = magicType
+        return copy
+    }
+}
+
+class NPCSpawner {
+    var npc: NPC
+    
+    init(npc: NPC) {
+        self.npc = npc
+    }
+    
+    func spawnNPC() -> NPC {
+        return npc.copy() as! NPC
+    }
+}
+
+let human = Human(name: "Bilal", type: "Warrior", level: 10, race: "Human")
+let elf = Elf(name: "Osama", type: "Mage", level: 5, magicType: "Fire")
+
+let humanSpawner = NPCSpawner(npc: human)
+let elfSpawner = NPCSpawner(npc: elf)
+
+let newHuman = humanSpawner.spawnNPC() as! Human
+let newElf = elfSpawner.spawnNPC() as! Elf
